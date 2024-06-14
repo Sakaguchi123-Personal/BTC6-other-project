f7172eac99fe9e2e3bd4b2db98aabf5e3568b39c
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-06-14 20:53:35 +0900

    add : calenderタブUIのみ追加

--- frontend/src/features/Calender/Calender.tsx
+++ frontend/src/features/Calender/Calender.tsx
@@ -1,16 +1,92 @@
-import { Center, Stack } from "@mantine/core";
-import { NavigateBar } from "../../components";
+import { Accordion, Center, ScrollArea, SegmentedControl, Stack } from "@mantine/core";
+import styled from "styled-components";
 import styles from "../../index.module.css";
 
+import { NavigateBar, SelectBarDate, TimeEdit } from "../../components";
+
 export const Calender = () => {
+  //--------------------------------------------------------------------------
+
+  const testDate = [
+    "01日",
+    "02日",
+    "03日",
+    "04日",
+    "05日",
+    "06日",
+    "07日",
+    "08日",
+    "09日",
+    "10日",
+    "11日",
+    "12日",
+  ];
+  //--------------------------------------------------------------------------
+
+  const items = testDate.map(item => (
+    <Accordion.Item key={item} value={item}>
+      <Accordion.Control>{item}</Accordion.Control>
+      <Accordion.Panel>
+        {
+          <>
+            <SegmentedControl
+              className={styles.toggle_switch}
+              color="brand"
+              // value={homeOrOffice}
+              // onChange={setHomeOrOffice}
+              data={[
+                {
+                  label: "在宅",
+                  value: "0",
+                },
+                {
+                  label: "出社",
+                  value: "1",
+                },
+              ]}
+            />
+            <TimeEdit startTime="08:00" endTime="18:00" startTitle="開始" endTitle="終了" />
+          </>
+        }
+      </Accordion.Panel>
+    </Accordion.Item>
+  ));
+
+  //--------------------------------------------------------------------------
   return (
     <>
-      <Stack align="center" justify="center" h={"100vh"}>
-        <h2 className={styles.text}>calenderタブ 未実装</h2>
+      <Stack align="center" justify="center">
+        <h2 className={styles.text}>calenderタブ 未完成</h2>
       </Stack>
+
+      <SelectBarDate title={"2024年06月"} clickLeftArrow={() => {}} clickRightArrow={() => {}} />
+      <StyleDiv>
+        <StyleP>稼働日: 20日</StyleP>
+        <StyleP>合計残業時間: 25:30</StyleP>
+        <StyleP>残業指示:30:00</StyleP>
+      </StyleDiv>
+
+      <ScrollArea h={410} scrollbarSize={6}>
+        <Accordion variant="separated" defaultValue="Apples" m={"20px"}>
+          {items}
+        </Accordion>
+      </ScrollArea>
+
       <Center>
         <NavigateBar className={styles.nav_bar} select="calender" />
       </Center>
     </>
   );
 };
+
+const StyleDiv = styled.div`
+  background-color: #ddb72d;
+  border-radius: 25px;
+  padding: 20px;
+  margin: 0 30px;
+`;
+const StyleP = styled.p`
+  margin: 0;
+  color: white;
+  font-size: 20px;
+`;
--- frontend/src/test/ButtonBasic.test.tsx
+++ frontend/src/test/ButtonBasic.test.tsx
@@ -1,18 +1,27 @@
 import { MantineProvider } from "@mantine/core";
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import "../../matchMedia.mock";
 
 import { ButtonBasic } from "../components";
 
 describe("Button", () => {
-  it("buttonがレンダリングされる", () => {
+  it("buttonがレンダリングされる", async () => {
+    const onClickMock = jest.fn();
     render(
       <MantineProvider>
-        <ButtonBasic>children</ButtonBasic>
+        <ButtonBasic click={onClickMock}>children</ButtonBasic>
       </MantineProvider>
     );
+    //レンダリングされたbuttonを取得
     const element = screen.getByRole("button");
     expect(element).toBeInTheDocument();
+    //childrenに渡した文字があるかテスト
     expect(element).toHaveTextContent("children");
+
+    //queryを使用し、buttonをクリックする
+    await userEvent.click(element);
+    //クリックしたときonClickMockが1回実行されたかをテスト
+    expect(onClickMock).toHaveBeenCalledTimes(1);
   });
 });
